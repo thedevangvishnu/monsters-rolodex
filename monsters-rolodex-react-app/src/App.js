bf66f3d2b9be38bffd25e8ef64450075e215ea0c
thedevangvishnu <thedevangvishnu@gmail.com>
2023-10-10 10:14:04 +0530

    Convert App into class component
    In App.js, refactor App component into class component and test your
    understanding by playing with basic hard-coded state values and
    updating state using event handlers.
    
    Remove strict mode in index.js.

--- monsters-rolodex-react-app/src/App.js
+++ monsters-rolodex-react-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    // console.log("constructor");
+    super();
+
+    this.state = {
+      monsters: [
+        { name: "Devang", id: 1 },
+        { name: "Naman", id: 2 },
+        { name: "Abheest", id: 3 },
+      ],
+    };
+  }
+
+  render() {
+    // console.log("render");
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.monsters.map((monster) => {
+            return <h2 key={monster.id}>{monster.name}</h2>;
+          })}
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
